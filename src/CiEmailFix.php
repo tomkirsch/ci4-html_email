fd065aaec0a94b13f7371768c2936b5c3655bdd7
tomkirsch <mrmotts@gmail.com>
2022-04-15 18:03:52 -0400

    formatting, made constructor param optional

--- src/CiEmailFix.php
+++ src/CiEmailFix.php
@@ -1,11 +1,16 @@
-<?php namespace Tomkirsch\HtmlEmail;
+<?php
+
+namespace Tomkirsch\HtmlEmail;
 /*
 	PHP mail() will always fail with CI's weird validation method. This class simply overwrites, and thus should properly set the -f flag
 */
+
 use CodeIgniter\Email\Email;
 
-class CiEmailFix extends Email{
-	protected function validateEmailForShell(&$email){
+class CiEmailFix extends Email
+{
+	protected function validateEmailForShell(&$email)
+	{
 		return TRUE;
 	}
 }
